4bf99a6e5fc34fe978284d33c74cb0506c85ef9a
Hunam <hunam@disroot.org>
2024-03-27 15:36:07 +0000

    rename `backend` to `app` Having backend as a Django app doesn't actually make sense because the frontend will also be in the same app, so having fronend code in an app called `backend` doesn't make sense

--- transcendence/asgi.py
+++ transcendence/asgi.py
@@ -12,7 +12,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from backend.urls import ws_urlpatterns
+from app.urls import ws_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transcendence.settings")
 django_asgi_app = get_asgi_application()
--- transcendence/settings.py
+++ transcendence/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "daphne",
-    "backend",
+    "app",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
