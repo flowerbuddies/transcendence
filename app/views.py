f86449771bec943fdbbd0702e85c4312496f4024
Hunam (Elie TD) <hunam@disroot.org>
2024-04-02 18:43:19 +0300

    feat(join): add game joining logic, validate the form on the server (#7)

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import re
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
 
@@ -9,13 +11,13 @@ def index(request):
         {
             "games": [
                 {
-                    "name": "Game 1",
+                    "name": "first_game",
                     "type": "Tournament 1v1v1v1",
                     "current_players": 2,
                     "max_players": 4,
                 },
                 {
-                    "name": "Game 2",
+                    "name": "Sec-game",
                     "type": "Game 1v1v1v1",
                     "current_players": 4,
                     "max_players": 4,
@@ -23,3 +25,59 @@ def index(request):
             ]
         },
     )
+
+
+def join(request: HttpRequest):
+    fields = request.POST.dict()
+
+    # check fields
+    if not all(
+        key in fields for key in {"game-name", "type", "players", "player-name"}
+    ):
+        return HttpResponseBadRequest("Not all fields specified")
+
+    # validate the `game-name` field
+    if not bool(re.compile(r"^[\w\-\.]{1,100}$").match(fields["game-name"])):
+        return HttpResponseBadRequest(
+            "Game name must be a valid unicode string with length < 100 containing only ASCII alphanumerics, hyphens, underscores, or periods"
+        )
+
+    # validate the `player-name` field
+    if len(fields["player-name"]) == 0:
+        return HttpResponseBadRequest("Player name must not be empty")
+
+    # validate the `type` & `players` fields
+    # only check them if creating a new game
+    # we detect if a new game is created by checking if `players` is an integer
+    # (eg: new game="4"; existing game="2/4")
+    try:
+        player_count = int(fields["players"])
+        match fields["type"]:
+            case "Game 1v1", "Game 1vAI":
+                if player_count != 2:
+                    return HttpResponseBadRequest(
+                        "Player count must be 2 for this game mode"
+                    )
+            case "Game 1v1v1v1", "Game 1v1vAIvAI":
+                if player_count != 4:
+                    return HttpResponseBadRequest(
+                        "Player count must be 4 for this game mode"
+                    )
+            case "Tournament 1v1":
+                if player_count % 2 != 0:
+                    return HttpResponseBadRequest(
+                        "Player count must be a modulo of 2 for this game mode"
+                    )
+            case "Tournament 1v1v1v1":
+                if player_count % 4 != 0:
+                    return HttpResponseBadRequest(
+                        "Player count must be a modulo of 4 for this game mode"
+                    )
+            case _:
+                return HttpResponseBadRequest("Invalid game mode")
+    except ValueError:
+        pass
+
+    # TODO: check if the game is full
+
+    return HttpResponse()
