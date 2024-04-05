a12afc2655860d0f741b03f00417bfbd5d82f3a2
Noel Lonka <93792522+Rubidium7@users.noreply.github.com>
2024-04-05 16:53:39 +0300

    feat(db): init db models (#9)

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Lobby(models.Model):
+	name = models.CharField(max_length=100)
+	is_tournament = models.BooleanField(default=False)
+	is_match_four = models.BooleanField(default=False)
+	has_started = models.BooleanField(default=False)
+	max_players = models.IntegerField()
+	def __str__(self):
+		return self.name
+
+class Player(models.Model):
+	game = models.ForeignKey(Lobby, on_delete=models.CASCADE)
+	name = models.CharField(max_length=12)
+	is_eliminated = models.BooleanField(default=False)
+	is_ai = models.BooleanField(default=False)
+	def __str__(self):
+		return self.name
