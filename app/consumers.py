a3c61f23aa7366b2bcb7e3069a4d498c9ae33b60
Hunam (Elie TD) <hunam@disroot.org>
2024-04-09 13:53:18 +0300

    fix(all): rename all relevent references of `game` to `lobby` (#12)

--- app/consumers.py
+++ app/consumers.py
@@ -4,18 +4,18 @@ from urllib.parse import unquote
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class GameConsumer(AsyncWebsocketConsumer):
+class LobbyConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.game_name = self.scope["url_route"]["kwargs"]["game_name"]
+        self.lobby_name = self.scope["url_route"]["kwargs"]["lobby_name"]
 
-        await self.channel_layer.group_add(self.game_name, self.channel_name)
+        await self.channel_layer.group_add(self.lobby_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.game_name, self.channel_name)
+        await self.channel_layer.group_discard(self.lobby_name, self.channel_name)
 
     async def receive(self, text_data):
-        await self.channel_layer.group_send(self.game_name, json.loads(text_data))
+        await self.channel_layer.group_send(self.lobby_name, json.loads(text_data))
 
     async def key(self, event):
         # for now just send back the key pressed
--- app/static/app/game.js
+++ app/static/app/game.js
@@ -1,82 +1,82 @@
-export function join(gameName, playerName) {
-  document.getElementById('join-view').classList.add('d-none');
-  document.getElementById('game-view').classList.remove('d-none');
+export function join(lobbyName, playerName) {
+    document.getElementById('join-view').classList.add('d-none');
+    document.getElementById('game-view').classList.remove('d-none');
 
-  // TODO: add support for 2 players on the same window
-  const key1 = 'q';
-  const key2 = 'a';
+    // TODO: add support for 2 players on the same window
+    const key1 = 'q';
+    const key2 = 'a';
 
-  const gameSocket = new WebSocket(
-    'ws://' + window.location.host + '/ws/game/' + gameName + '/'
-  );
+    const gameSocket = new WebSocket(
+        'ws://' + window.location.host + '/ws/lobby/' + lobbyName + '/'
+    );
 
-  gameSocket.onmessage = (e) => {
-    console.log(JSON.parse(e.data));
-  };
+    gameSocket.onmessage = (e) => {
+        console.log(JSON.parse(e.data));
+    };
 
-  gameSocket.onclose = (e) => {
-    console.error('Game ws closed');
-  };
+    gameSocket.onclose = (e) => {
+        console.error('Game WS closed');
+    };
 
-  let isKey1Pressed = false;
-  let isKey2Pressed = false;
+    let isKey1Pressed = false;
+    let isKey2Pressed = false;
 
-  function keyPressed(ev) {
-    if (ev.key === key1) {
-      // send only one key event on `keydown`
-      if (!isKey1Pressed)
-        gameSocket.send(
-          JSON.stringify({
-            type: 'key',
-            key: 1,
-            player: playerName,
-          })
-        );
-      isKey1Pressed = true;
-    } else if (ev.key === key2) {
-      // send only one key event on `keydown`
-      if (!isKey2Pressed)
-        gameSocket.send(
-          JSON.stringify({
-            type: 'key',
-            key: 2,
-            player: playerName,
-          })
-        );
-      isKey2Pressed = true;
+    function keyPressed(ev) {
+        if (ev.key === key1) {
+            // send only one key event on `keydown`
+            if (!isKey1Pressed)
+                gameSocket.send(
+                    JSON.stringify({
+                        type: 'key',
+                        key: 1,
+                        player: playerName,
+                    })
+                );
+            isKey1Pressed = true;
+        } else if (ev.key === key2) {
+            // send only one key event on `keydown`
+            if (!isKey2Pressed)
+                gameSocket.send(
+                    JSON.stringify({
+                        type: 'key',
+                        key: 2,
+                        player: playerName,
+                    })
+                );
+            isKey2Pressed = true;
+        }
     }
-  }
 
-  function keyReleased(ev) {
-    if (ev.key === key1) {
-      gameSocket.send(
-        JSON.stringify({
-          type: 'key',
-          key: 1,
-          player: playerName,
-        })
-      );
-      isKey1Pressed = false;
-    } else if (ev.key === key2) {
-      gameSocket.send(
-        JSON.stringify({
-          type: 'key',
-          key: 2,
-          player: playerName,
-        })
-      );
-      isKey2Pressed = false;
+    function keyReleased(ev) {
+        if (ev.key === key1) {
+            gameSocket.send(
+                JSON.stringify({
+                    type: 'key',
+                    key: 1,
+                    player: playerName,
+                })
+            );
+            isKey1Pressed = false;
+        } else if (ev.key === key2) {
+            gameSocket.send(
+                JSON.stringify({
+                    type: 'key',
+                    key: 2,
+                    player: playerName,
+                })
+            );
+            isKey2Pressed = false;
+        }
     }
-  }
 
-  // we send the same event for both `keydown` and `keyup`
-  // depending on the previous state, the server will be able to understand what's happening
-  document.addEventListener('keydown', keyPressed);
-  document.addEventListener('keyup', keyReleased);
+    // we send the same event for both `keydown` and `keyup`
+    // depending on the previous state, the server will be able to understand what's happening
+    document.addEventListener('keydown', keyPressed);
+    document.addEventListener('keyup', keyReleased);
 
-  // TODO: for now just print a black square
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // TODO: for now just print a black square
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
