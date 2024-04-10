c46caab46ae95a0089de1bd3f1422f7c223464ac
Hunam <hunam@disroot.org>
2024-04-10 20:48:17 +0000

    feat(join,game): handle back & forward buttons

--- app/static/app/game.js
+++ app/static/app/game.js
@@ -1,46 +1,69 @@
-// TODO: add support for 2 players on the same window
-const key1 = 'q';
-const key2 = 'a';
+export let gameSocket;
 
-const gameSocket = new WebSocket(
-    'ws://' + window.location.host + '/ws/lobby/' + lobbyName + '/'
-);
+function initConn(lobbyName, playerName) {
+    // TODO: add support for 2 players on the same window
+    const key1 = 'q';
+    const key2 = 'a';
 
-gameSocket.onopen = () => {
-    gameSocket.send(JSON.stringify({ type: 'init', player: playerName }));
-};
+    gameSocket = new WebSocket(
+        'ws://' + window.location.host + '/ws/lobby/' + lobbyName + '/'
+    );
 
-gameSocket.onmessage = (e) => {
-    const data = JSON.parse(e.data);
+    gameSocket.onopen = () => {
+        gameSocket.send(JSON.stringify({ type: 'init', player: playerName }));
+    };
 
-    if (data.type == 'players') {
-        // players connected
-        document.getElementById(
-            'players-connected'
-        ).textContent = `${data.players.length} / ${data.max}`;
+    gameSocket.onmessage = (e) => {
+        const data = JSON.parse(e.data);
 
-        // players list
-        const list = document.getElementById('players-list');
-        let content = '';
-        for (const player of data.players)
-            content += `<li class="list-group-item list-group-item-${
-                player.is_eliminated ? 'danger' : 'success'
-            }">${player.name} ${player.is_ai ? '🤖' : ''}</li>`;
-        list.innerHTML = content;
+        if (data.type == 'players') {
+            // players connected
+            document.getElementById(
+                'players-connected'
+            ).textContent = `${data.players.length} / ${data.max}`;
+
+            // players list
+            const list = document.getElementById('players-list');
+            let content = '';
+            for (const player of data.players)
+                content += `<li class="list-group-item list-group-item-${
+                    player.is_eliminated ? 'danger' : 'success'
+                }">${player.name} ${player.is_ai ? '🤖' : ''}</li>`;
+            list.innerHTML = content;
+        }
+    };
+
+    let isKey1Pressed = false;
+    let isKey2Pressed = false;
+
+    function keyPressed(ev) {
+        if (ev.key === key1) {
+            // send only one key event on `keydown`
+            if (!isKey1Pressed)
+                gameSocket.send(
+                    JSON.stringify({
+                        type: 'key',
+                        key: 1,
+                        player: playerName,
+                    })
+                );
+            isKey1Pressed = true;
+        } else if (ev.key === key2) {
+            // send only one key event on `keydown`
+            if (!isKey2Pressed)
+                gameSocket.send(
+                    JSON.stringify({
+                        type: 'key',
+                        key: 2,
+                        player: playerName,
+                    })
+                );
+            isKey2Pressed = true;
+        }
     }
-};
 
-gameSocket.onclose = (e) => {
-    console.error('Game WS closed');
-};
-
-let isKey1Pressed = false;
-let isKey2Pressed = false;
-
-function keyPressed(ev) {
-    if (ev.key === key1) {
-        // send only one key event on `keydown`
-        if (!isKey1Pressed)
+    function keyReleased(ev) {
+        if (ev.key === key1) {
             gameSocket.send(
                 JSON.stringify({
                     type: 'key',
@@ -48,10 +71,8 @@ function keyPressed(ev) {
                     player: playerName,
                 })
             );
-        isKey1Pressed = true;
-    } else if (ev.key === key2) {
-        // send only one key event on `keydown`
-        if (!isKey2Pressed)
+            isKey1Pressed = false;
+        } else if (ev.key === key2) {
             gameSocket.send(
                 JSON.stringify({
                     type: 'key',
@@ -59,39 +80,25 @@ function keyPressed(ev) {
                     player: playerName,
                 })
             );
-        isKey2Pressed = true;
+            isKey2Pressed = false;
+        }
     }
+
+    // we send the same event for both `keydown` and `keyup`
+    // depending on the previous state, the server will be able to understand what's happening
+    document.addEventListener('keydown', keyPressed);
+    document.addEventListener('keyup', keyReleased);
 }
 
-function keyReleased(ev) {
-    if (ev.key === key1) {
-        gameSocket.send(
-            JSON.stringify({
-                type: 'key',
-                key: 1,
-                player: playerName,
-            })
-        );
-        isKey1Pressed = false;
-    } else if (ev.key === key2) {
-        gameSocket.send(
-            JSON.stringify({
-                type: 'key',
-                key: 2,
-                player: playerName,
-            })
-        );
-        isKey2Pressed = false;
-    }
+function initScene() {
+    // TODO: for now just print a black square
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
-// we send the same event for both `keydown` and `keyup`
-// depending on the previous state, the server will be able to understand what's happening
-document.addEventListener('keydown', keyPressed);
-document.addEventListener('keyup', keyReleased);
-
-// TODO: for now just print a black square
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'black';
-ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+export function joinLobby(lobbyName, playerName) {
+    initConn(lobbyName, playerName);
+    initScene();
+}
---
+++ app/static/app/join.js
@@ -0,0 +1,26 @@
+import { joinLobby } from '/static/app/game.js';
+import { setBody } from '/static/app/index.js';
+
+export function registerJoinForms() {
+    document.querySelectorAll('.form-join').forEach((el) =>
+        el.addEventListener('submit', async (ev) => {
+            // prevent a page refresh
+            ev.preventDefault();
+
+            // register the join/create
+            const formData = new FormData(ev.target);
+            const joinRes = await fetch('/join', {
+                method: 'POST',
+                body: formData,
+            });
+            if (joinRes.status != 200) return alert(await joinRes.text());
+
+            // switch to game view
+            const lobbyName = formData.get('lobby-name');
+            const playerName = formData.get('player-name');
+            await setBody(`/game?lobby=${lobbyName}&player=${playerName}`);
+            joinLobby(lobbyName, playerName);
+            history.pushState('game', null);
+        })
+    );
+}
