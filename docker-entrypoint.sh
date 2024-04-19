03b00519b810447e1fa4da612f98a8a6ea852915
Markus Laaksonen <markus@mxa.fi>
2024-04-19 17:48:13 +0300

    add basic init tasks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,13 @@ cd /app
 python3 manage.py makemigrations
 python3 manage.py migrate
 
+cat <<EOF | python manage.py shell
+from django.contrib.auth import get_user_model
+User = get_user_model()
+User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists() or \
+    User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')
+EOF
+
+django-admin compilemessages
+
 exec python3 manage.py runserver 0.0.0.0:8000
