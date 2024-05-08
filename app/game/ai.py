5c0136c3e9bbd617281236dcd2fc87bddc8d9de0
marmulle <marmulle@student.hive.fi>
2024-05-08 11:14:26 +0000

    feat(ai): add tunable hesitation and inaccuracy; update ai difficulty on score change; allow paddle to move near predicted intercept to improve game feel

--- app/game/ai.py
+++ app/game/ai.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 
 
 class BehaviorTree:
@@ -6,35 +7,49 @@ class BehaviorTree:
         self.gs = game_state
         self.player = player
         self.difficulty = 0.0
-        self.inaccuracy = 0.0
+        self.accumulated_scores = 0
+        self.hesitation_seconds = 1
         self.hesitation_start = asyncio.get_event_loop().time()
-        self.hesitation_seconds = 3
-        self.predict_x = 0.0
-        self.predict_y = 0.0
+        self.inaccuracy_max = 0.0
+        self.inaccuracy = 0.0
+        self.predict_x = 0.5
+        self.predict_y = 0.5
 
+    # called once per frame
     def update(self):
-        # called once per frame
         if not self.is_defending():
             self.hesitation_start = asyncio.get_event_loop().time()
             return
-        if self.is_hesitating():
-            return
-        self.set_difficulty()
-        self.apply_difficulty()
+        if self.did_scores_change():
+            self.set_difficulty()
+            self.apply_difficulty()
         self.predict()
-        self.move_to_ball()
+        if not self.is_hesitating():
+            self.move_to_prediction()
 
+    # TODO: depends on score/life implementation
+    # set difficulty using gs, e.g. scores
     def set_difficulty(self):
-        # set difficulty using gs, e.g. scores
-        # TODO: depends on score/life implementation
+        # something = equation(self.player.score, self.accumulated_scores)
+        # self.inaccuracy_max = something * {0, 1}
+        # self.hesitation_seconds = something * {0, max_seconds}
         pass
 
+    # using difficulty, set hesitation and inaccuracy
     def apply_difficulty(self):
-        # using difficulty, set hesitation and inaccuracy
-        pass
+        self.inaccuracy = random.uniform(-self.inaccuracy_max, self.inaccuracy_max)
 
+    def did_scores_change(self):
+        previous_accumulated_scores = self.accumulated_scores
+        self.accumulated_scores = self.gs.left.score
+        self.accumulated_scores += self.gs.right.score
+        if self.gs.isFourPlayer:
+            self.accumulated_scores += self.gs.top.score
+            self.accumulated_scores += self.gs.bottom.score
+        return previous_accumulated_scores != self.accumulated_scores
+
+    # true if ball is approaching ai paddle
     def is_defending(self):
-        # true if ball is approaching ai paddle
         return (
             self.player.side == "left"
             and self.gs.ball.dx < 0
@@ -46,57 +61,72 @@ class BehaviorTree:
             and self.gs.ball.dy > 0
         )
 
+    # returns true if still hesitating
     def is_hesitating(self):
-        # returns true if still hesitating
         return (
             asyncio.get_event_loop().time()
             < self.hesitation_start + self.hesitation_seconds
         )
 
+    # predict where to move, using inaccuracy
     def predict(self):
-        # predict where to move, using inaccuracy
-        # TODO: use inaccuracy
-        self.get_intercept()
+        self.set_prediction()
+        self.predict_x += self.inaccuracy
+        self.predict_y += self.inaccuracy
+        offset = self.player.paddle.margin + 0.5 * self.player.paddle.length
+        self.predict_x = clamp(self.predict_x, offset, 1.0 - offset)
+        self.predict_y = clamp(self.predict_y, offset, 1.0 - offset)
 
     def get_gradient(self):
         if self.gs.ball.dx == 0.0:
             return 1e5
         return self.gs.ball.dy / self.gs.ball.dx
 
-    def get_intercept(self):
+    def set_prediction(self):
+        offset = self.player.paddle.margin + self.player.paddle.depth
         gradient = self.get_gradient()
         if gradient == 0.0:
             gradient = 1e-4
-        # TODO: check the math...
         if self.player.side == "left":
-            self.predict_x = 0
-            self.predict_y = self.gs.ball.y - gradient * self.gs.ball.x
+            self.predict_y = self.gs.ball.y + gradient * (offset - self.gs.ball.x)
         elif self.player.side == "right":
-            self.predict_x = 1
-            self.predict_y = self.gs.ball.y + gradient * (1 - self.gs.ball.x)
+            self.predict_y = self.gs.ball.y + gradient * (1 - offset - self.gs.ball.x)
         elif self.player.side == "top":
-            self.predict_x = (0 - self.gs.ball.y) / gradient + self.gs.ball.x
-            self.predict_y = 0
+            self.predict_x = (offset - self.gs.ball.y) / gradient + self.gs.ball.x
         elif self.player.side == "bottom":
-            self.predict_x = (1 - self.gs.ball.y) / gradient + self.gs.ball.x
-            self.predict_y = 1
+            self.predict_x = (1 - offset - self.gs.ball.y) / gradient + self.gs.ball.x
 
-    def move_to_ball(self):
-        # move paddle to intercept ball
-        # TODO: fix "wiggle"
-        self.player.paddle.is_up_pressed = False
-        self.player.paddle.is_down_pressed = False
+    # move paddle to intercept ball
+    # start hesitation when reaching prediction
+    def move_to_prediction(self):
+        paddle = self.player.paddle
+        paddle.is_up_pressed = False
+        paddle.is_down_pressed = False
         if self.player.side == "left" or self.player.side == "right":
-            if self.predict_y < self.player.paddle.y + 0.5 * self.player.paddle.length:
-                self.player.paddle.is_up_pressed = True
-            elif (
-                self.predict_y > self.player.paddle.y + 0.5 * self.player.paddle.length
-            ):
-                self.player.paddle.is_down_pressed = True
+            if self.is_nearby(self.predict_y, paddle.y):
+                self.hesitation_start = asyncio.get_event_loop().time()
+            else:
+                if self.predict_y < paddle.y + 0.5 * paddle.length:
+                    paddle.is_up_pressed = True
+                else:
+                    paddle.is_down_pressed = True
         else:
-            if self.predict_x < self.player.paddle.x + 0.5 * self.player.paddle.length:
-                self.player.paddle.is_up_pressed = True
-            elif (
-                self.predict_x > self.player.paddle.x + 0.5 * self.player.paddle.length
-            ):
-                self.player.paddle.is_down_pressed = True
+            if self.is_nearby(self.predict_x, paddle.x):
+                self.hesitation_start = asyncio.get_event_loop().time()
+            else:
+                if self.predict_x < paddle.x + 0.5 * paddle.length:
+                    paddle.is_up_pressed = True
+                else:
+                    paddle.is_down_pressed = True
+
+    # check if paddle is near prediction
+    def is_nearby(self, predict, paddle):
+        return (
+            paddle + 0.2 * self.player.paddle.length
+            < predict
+            < paddle + 0.8 * self.player.paddle.length
+        )
+
+
+# constrain 'value' to between 'min_value' and 'max_value'
+clamp = lambda value, min_value, max_value: max(min(value, max_value), min_value)
