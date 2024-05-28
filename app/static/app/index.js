0d4f00932820764f321c8b04e1c927fe8019bc8d
Rubidium7 <kyllikki.fillman@gmail.com>
2024-05-28 15:24:59 +0000

    fix(history): forward and backward buttons work

--- app/static/app/index.js
+++ app/static/app/index.js
@@ -7,15 +7,11 @@ registerPlayerOptionsUpdate();
 const closeWSConnsEvent = new Event("closeWSConns");
 
 window.onpopstate = async (event) => {
-    console.log(event.state);
     if (event.state === "join") {
         await setBody("/join");
         registerJoinForms();
         registerPlayerOptionsUpdate();
         document.dispatchEvent(closeWSConnsEvent);
-    }
-    if (event.state === "game") {
-        //
     }
 };
 
